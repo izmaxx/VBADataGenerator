8aac3256b57a3637bd63c2d684d46421ed9da29b
izmaxx <isunhachawi@gmail.com>
2015-11-20 09:47:37 -0500

    Solver interface
    Added interface class to define what solver portion needs to return and
    what it takes as input

---
+++ ANTLRTest/ISolver.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANTLRTest
+{
+    interface ISolver
+    {
+        /// <summary>
+        /// Takes in a path constraint (one or more conditions) and returns inputs that will satisfy the path constraint.
+        /// </summary>
+        /// <param name="pathConstraints">Expression object that contains path constraint</param>
+        /// <returns>Assignment of variables to inputs that satisfy constraints </returns>
+        Dictionary<String, Object> calculateTestData(Expression pathConstraints);
+
+        Boolean checkSatisfiability(Expression pathConstraints);
+
+        /* This is an example Expression */
+        // (p1 < 3 or p2 > 3) and (p2 <= 3)
+
+        //ParameterExpression p1 = Expression.Parameter(typeof(int), "p1");
+        //ParameterExpression p2 = Expression.Parameter(typeof(int), "p2");
+        //ConstantExpression c1 = Expression.Constant(3);
+        //BinaryExpression b1 = Expression.LessThan(p1, c1);
+        //BinaryExpression b2 = Expression.GreaterThan(p2, c1);
+        //BinaryExpression b3 = Expression.LessThanOrEqual(p2, c1);
+        //BinaryExpression exp = Expression.And(Expression.Or(b1, b2), b3);
+        
+    }
+}
--- ANTLRTest/Program.cs
+++ ANTLRTest/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
+using System.Linq.Expressions;
 
 namespace ANTLRTest
 {
@@ -23,12 +24,27 @@ namespace ANTLRTest
             VBGrammarParser parser = new VBGrammarParser(tokens);
 
             IParseTree tree = parser.startRule(); // begin parsing at init rule
+            
             Console.WriteLine(tree.ToStringTree(parser));
 
+            ParameterExpression p1 = Expression.Parameter(typeof(int), "p1");
+            ParameterExpression p2 = Expression.Parameter(typeof(int), "p2");
+            ConstantExpression c1 = Expression.Constant(3);
+            BinaryExpression b1 = Expression.LessThan(p1, c1);
+            BinaryExpression b2 = Expression.GreaterThan(p2, c1);
+            BinaryExpression b3 = Expression.LessThanOrEqual(p2, c1);
+            BinaryExpression final = Expression.And(Expression.Or(b1, b2), b3);
+
+            //var test = b1.Left.GetType();
+            System.Console.WriteLine(final);
+
+            List<Expression> exp = new List<Expression>();
            // LabeledExprVisitor eval = new LabeledExprVisitor();
            // eval.Visit(tree);
 
             //parser.addSubExpr();
+
+
         }
     }
 }
